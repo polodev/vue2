1d1e933af0b64decd00d2230565a4ba3fd5cf48a
polodev <polodev10@gmail.com>
2017-03-28 21:21:18 +0600

    added class 8 where vfor is described

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+Vue.component('task-list', {
+  template: `
+    <div>
+      <task v-for="task in tasks">{{task.name}}</task>
+    </div>
+  `,
+  data () {
+    return {
+        tasks : [
+        {name: 'Waking up', completed: true},
+        {name: 'Brash teeth', completed: true},
+        {name: 'take food', completed: true},
+        {name: 'work for long time', completed: false},
+        {name: 'do gym', completed: false},
+        {name: 'do bath', completed: false},
+      ]
+    }
+  }
+})
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 })
