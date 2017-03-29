dfcd32874bfe1fee6e06310bdddc46f6fa4b5971
polodev <polodev10@gmail.com>
2017-03-29 19:13:00 +0600

    added class 11, , props rule

--- script.js
+++ script.js
@@ -1,15 +1,58 @@
-Vue.component('modal', {
+Vue.component('tabs', {
   template: `
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-content">
-        <div class="box">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, adipisci!</p>
-        </div>
+    <div>
+      <div class="tabs">
+        <ul>
+          <li v-for="tab in tabs" :class="{ 'is-active ': tab.isActive }" @click="_selectTab(tab)">
+            <a :href="tab.href">{{ tab.name }}</a>
+          </li>
+        </ul>
       </div>
-      <button @click="$emit('close')" class="modal-close"></button>
+      <div class="tab-details">
+        <slot></slot>
+      </div>
     </div>
-  `
+  `,
+  data () {
+    return {
+      tabs: []
+    }
+  },
+  props: {
+  },
+  mounted () {
+    this.tabs = this.$children
+  },
+  methods: {
+    _selectTab(selectTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name == selectTab.name)
+      })
+    }
+  },
+
+})
+
+Vue.component('tab', {
+  template: `
+    <div v-show="isActive">
+      <slot></slot>
+    </div>
+  `,
+  props: {
+    name: {required: true},
+    selected: {default: false}
+  },
+  data () {
+    return {
+      isActive : this.selected,
+    }
+  },
+  computed: {
+    href () {
+      return "#" + this.name.toLowerCase().replace(/ /, "-");
+    }
+  }
 })
 
 new Vue({
