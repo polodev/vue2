d7a61702bc4f4a6adfbcbe0964ff188befcfc848
polodev <polodev10@gmail.com>
2017-03-30 11:06:30 +0600

    added file 12 for parents child communication

--- script.js
+++ script.js
@@ -1,71 +1,24 @@
-Vue.component('tabs', {
+Vue.component('coupon', {
   template: `
-    <div>
-      <div class="tabs">
-        <ul>
-          <li v-for="tab in tabs" :class="{ 'is-active ': tab.isActive }" @click="_selectTab(tab)">
-            <a :href="tab.href">{{ tab.name }}</a>
-          </li>
-        </ul>
-      </div>
-      <div class="tab-details">
-        <slot></slot>
-      </div>
-    </div>
+    <input type="text" @blur="localOnCouponApplied" />
   `,
-  data () {
-    return {
-      tabs: []
-    }
-  },
-  props: {
-  },
-  mounted () {
-    this.tabs = this.$children
-  },
   methods: {
-    _selectTab(selectTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = (tab.name == selectTab.name)
-      })
-    }
-  },
-
-})
-
-Vue.component('tab', {
-  template: `
-    <div v-show="isActive">
-      <slot></slot>
-    </div>
-  `,
-  props: {
-    name: {required: true},
-    selected: {default: false}
-  },
-  data () {
-    return {
-      isActive : this.selected,
-    }
-  },
-  computed: {
-    href () {
-      return "#" + this.name.toLowerCase().replace(/ /, "-");
+    localOnCouponApplied () {
+      this.$emit('applied')
     }
   }
 })
 
+
 new Vue({
   el: '#root',
   data: {
-    modal: false
+    modal: false,
+    isCouponApplied: false
   },
   methods: {
-    showModal () {
-      this.modal = true
-    },
-    hideModal () {
-      this.modal = false
+    onCouponApplied() {
+      this.isCouponApplied = true
     }
   }
 })
